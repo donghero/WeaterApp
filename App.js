c5c2e383217f80d4b22e1b2bf2957f7c4789fa03
donghero <2524480125@qq.com>
2020-06-22 17:09:42 +0900

    commit

--- App.js
+++ App.js
@@ -1,19 +1,53 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {Alert} from "react-native"
+import Loading from "./Loading";
+import * as Location from "expo-location";
+import axios from "axios";
+import Weather from "./Weather";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+    state = {
+        isLoading: true
+    };
+
+    render() {
+        const {isLoading, temp, condition} = this.state;
+        return (
+            isLoading ? <Loading/> : <Weather temp={Math.round(temp)} condition={condition} />
+        );
+    }
+
+    getWeather = async (latitude, longitude) => {
+        const {
+            data: {
+                main: {temp},
+                weather
+            }
+        } = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=26df358bbce22fdab6dc8e44bac26a6b&units=metric`);
+
+        this.setState({
+            isLoading: false,
+            temp,
+            condition: weather[0].main
+        })
+    };
+
+    getLocation = async () => {
+        try {
+            await Location.requestPermissionsAsync();
+            await Location.getPermissionsAsync();
+            const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync({accuracy: Location.Accuracy.Highest});
+            this.getWeather(latitude, longitude);
+        } catch (error) {
+            Alert.alert("Can not find ", "So sad");
+        }
+    };
+
+    componentDidMount() {
+        this.getLocation().then((res) => {
+        });
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
